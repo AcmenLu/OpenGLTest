83296b439d14d5999c28bb6f30d9ecf0de89dbe6
AcmenLu <1109325089@qq.com>
2018-03-31 21:08:50 +0800

    Add texture
    Add texture

--- OpenGLTest/TextureSample.cpp
+++ OpenGLTest/TextureSample.cpp
@@ -12,10 +12,13 @@ bool InitOpenGL( );
 void OnKeyCallback( GLFWwindow* window, int key, int scancode, int action, int mode );
 void Render( );
 void InitData( );
+unsigned int loadImage( char* filename );
 
 unsigned int VBO;
+unsigned int EBO;
 unsigned int VAO;
-unsigned int shaderProgram;
+unsigned int mTexture;
+unsigned int mTexture2;
 
 Shader* shader;
 
@@ -76,17 +79,25 @@ void OnKeyCallback( GLFWwindow* window, int key, int scancode, int action, int m
 
 void Render( )
 {
+	glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
+	glClear( GL_COLOR_BUFFER_BIT );
 	shader->Use( );
+	glActiveTexture( GL_TEXTURE0 );
+	glBindTexture( GL_TEXTURE_2D, mTexture );
+	glActiveTexture( GL_TEXTURE1 );
+	glBindTexture( GL_TEXTURE_2D, mTexture2 );
 	glBindVertexArray( VAO );
-	glDrawArrays( GL_TRIANGLES, 0, 3 );
+	glDrawElements( GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0 );
 }
 
 void InitData( )
 {
 	float vertices[] = {
-		0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
-		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
-		0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f
+		//     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
+		0.5f,  0.5f, 0.0f,	1.0f, 0.0f, 0.0f,	1.0f, 1.0f,   // 右上
+		0.5f, -0.5f, 0.0f,	0.0f, 1.0f, 0.0f,	1.0f, 0.0f,   // 右下
+		-0.5f, -0.5f, 0.0f,	0.0f, 0.0f, 1.0f,	0.0f, 0.0f,   // 左下
+		-0.5f, 0.5f, 0.0f,	1.0f, 1.0f, 0.0f,	0.0f, 1.0f    // 左上
 	};
 
 	glGenBuffers( 1, &VBO );
@@ -94,12 +105,49 @@ void InitData( )
 	glBindVertexArray( VAO );
 
 	glBindBuffer( GL_ARRAY_BUFFER, VBO );
-	glBufferData( GL_ARRAY_BUFFER, sizeof( vertices), vertices, GL_STATIC_DRAW );
+	glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
+
+	unsigned int indices[] = {
+		0, 1, 3,
+		1, 2, 3
+	};
+	glGenBuffers( 1, &EBO );
+	glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, EBO );
+	glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( indices ), indices, GL_STATIC_DRAW );
 
 	shader = new Shader( "./Shader/texturesample_vs.txt", "./Shader/texturesample_fs.txt" );
 
-	glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof( float ), (void*)0 );
+	glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof( float ), (void*)0 );
 	glEnableVertexAttribArray( 0 );
-	glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof( float ), (void*)( 3 * sizeof(float) ) );
+	glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof( float ), (void*)( 3 * sizeof(float) ) );
 	glEnableVertexAttribArray( 1 );
+	glVertexAttribPointer( 2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof( float ), (void*)( 6 * sizeof(float) ) );
+	glEnableVertexAttribArray( 2 );
+
+	mTexture = loadImage( "texture/container.jpg" );
+	mTexture2 = loadImage( "texture/awesomeface.png" );
+	shader->SetInt( "texture1", 0 );
+	shader->SetInt( "texture2", 1 );
+}
+
+unsigned int loadImage( char * filename )
+{
+	unsigned int texture;
+	glGenTextures( 1, &texture );
+	glBindTexture( GL_TEXTURE_2D, texture );
+
+	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
+	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT );
+	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
+	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
+
+	int width, height, nrChannels;
+	unsigned char* data = stbi_load( filename, &width, &height, &nrChannels, 0 );
+	if ( data )
+	{
+		glTexImage2D( GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data );
+		glGenerateMipmap( GL_TEXTURE_2D );
+	}
+	stbi_image_free( data );
+	return texture;
 }
